89d936ed1f3d95a0dc83f04571f0c8e822cdc948
Dan Korytov <dkorytov@uchicago.edu>
2019-09-19 14:38:23 -0400

    added link to website

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+print("========================================")
+print("Website should at http://localhost:5000/")
+print("========================================")
+print("\n")
+
 from application_folder import flask_instance
 
 flask_instance.run(debug=True)
